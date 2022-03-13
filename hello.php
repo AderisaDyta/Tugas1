921fd3a021ffdf48a1fcfe8132f20c3724573c59
AderisaDyta <200411100013@student.trunojoyo.ac.id>
2022-03-13 13:21:42 +0700

    menambah file hello.php

---
+++ hello.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Nomor 1</title>
+</head>
+<body>
+    <center>
+    <!--//Fungsi dari Echo untuk menampilkan teks ke dalam layar. -->
+    <h2>Nama : <?php echo "Aderisa Dyta Okvianti";?><h2> 
+    <h2>Nim : <?php echo "200411100013";?><h2>
+</center>
+</body>
+</html>
